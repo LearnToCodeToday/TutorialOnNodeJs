18c5417c080808b37c3e020ba2fa241a7dec7723
shiva <sadhikaria@gmail.com>
2015-05-29 10:26:50 -0400

    nodejs tutorial commit

---
+++ TutorialOnNodeJs/gulpfile.js
@@ -0,0 +1,47 @@
+﻿var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+
+var uglyFy = require('gulp-uglify');
+
+
+
+
+gulp.task('copyEjsFile', function () {
+    gulp.src('views/*.ejs')
+    .pipe(gulp.dest('site/'));
+   
+});
+
+gulp.task('compress', function () {
+
+    return
+        gulp.src('pre-js/*.js')
+            .pipe(uglyFy())
+            .pipe(gulp.dest('post-js'))
+})
+
+gulp.task("default", function () {
+    
+    
+   
+    
+
+    nodemon({
+        
+        
+        script: 'app.js',
+        ext : 'js',
+        env : {
+            PORT : 9000
+        }
+        ,
+        ignore: ['./node_modules/**']
+    })
+    .on('restart', function () {
+        console.log("Restarting the server");
+
+    });
+
+
+    
+})
